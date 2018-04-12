322750a09da7e1cda5ba4ff245d49f319188c09e
MSI <MSI@MSI-PC>
2018-04-12 22:32:24 +0300

    Добавьте файлы проекта.

---
+++ GroupsDeleteBanned/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace GroupsDeleteBanned
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.vkToken = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gid
+            // 
+            this.gid.Location = new System.Drawing.Point(12, 35);
+            this.gid.Name = "gid";
+            this.gid.Size = new System.Drawing.Size(125, 20);
+            this.gid.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GroupID (только цифры)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AccsessToken";
+            // 
+            // vkToken
+            // 
+            this.vkToken.Location = new System.Drawing.Point(12, 101);
+            this.vkToken.Name = "vkToken";
+            this.vkToken.Size = new System.Drawing.Size(244, 20);
+            this.vkToken.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить настройки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 189);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(464, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(158, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Удалить собак";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 229);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vkToken);
+            this.Controls.Add(this.gid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VK Group Delete all banned users";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox gid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox vkToken;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GroupsDeleteBanned/Form1.cs
@@ -0,0 +1,214 @@
+﻿/*Copyright Hermein Developer
+ License CC BY-SA 4.0 
+ 06.04.2018 */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using System.Windows.Forms;
+
+namespace GroupsDeleteBanned
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        public static string VkGid;
+        public static string VkToken;
+        public static List<string> Lst;
+        public static int Usercount = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://oauth.vk.com/authorize?client_id=6438972&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=groups,offline&response_type=token&v=5.74");
+        }
+        public bool Validator()
+        {
+            int a = 0;
+            if (int.TryParse(gid.Text, out a))
+            {
+                if (!string.IsNullOrWhiteSpace(vkToken.Text))
+                    VkGid = a.ToString();
+                VkToken = vkToken.Text;
+                return true;
+            }
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Validator())
+            {
+                Properties.Settings.Default.gid = VkGid;
+                Properties.Settings.Default.token = VkToken;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("Неверно заполнены поля ввода", "Ошибка");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.gid))
+            {
+                VkGid = Properties.Settings.Default.gid;
+                gid.Text = VkGid;
+            }
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.token))
+            {
+                VkToken = Properties.Settings.Default.token;
+                vkToken.Text = VkToken;
+            }
+        }
+        private static string Post(string url, string post)
+        {
+            ServicePointManager.ServerCertificateValidationCallback =
+                     ((sender, certificate, chain, sslPolicyErrors) => true);
+            var html = string.Empty;
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent =
+                    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.102 YaBrowser/14.2.1700.12599 Safari/537.36";
+                request.Method = "POST";
+                request.AllowAutoRedirect = true;
+                request.Timeout = 60000;
+           
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Accept = "*/*";
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate,sdch");
+                request.Headers.Add(HttpRequestHeader.AcceptLanguage, "ru,en;q=0.8");
+                var encodedPostParams = Encoding.Default.GetBytes(post);
+                request.ContentLength = encodedPostParams.Length;
+                request.GetRequestStream().Write(encodedPostParams, 0, encodedPostParams.Length);
+                request.GetRequestStream().Close();
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseStream = response.GetResponseStream();
+                if (responseStream != null)
+                {
+                    if (response.ContentEncoding.ToLower().Contains("gzip"))
+                        responseStream = new System.IO.Compression.GZipStream(responseStream, System.IO.Compression.CompressionMode.Decompress);
+                    else if (response.ContentEncoding.ToLower().Contains("deflate"))
+                        responseStream = new System.IO.Compression.DeflateStream(responseStream, System.IO.Compression.CompressionMode.Decompress);
+                    var sRead = new StreamReader(responseStream, Encoding.Default);
+                    html = sRead.ReadToEnd();
+                    sRead.Close();
+                    sRead.Dispose();
+                    responseStream.Close();
+                    responseStream.Dispose();
+                }
+            }
+            catch
+            {
+
+            }
+            return html;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Validator())
+            {
+                button3.Enabled = false;
+                Lst = new List<string>();
+                Usercount = 999;
+                progressBar1.Value = 0;
+                new Thread(() => {
+                    for (var i = 0; i < 100000; i++)
+                    {
+                        var offset = (i * 1000);
+                        if (Usercount <= 0 || offset >= Usercount)
+                        {
+                            progressBar1.Value = progressBar1.Maximum;
+                            break;
+                        }
+                        var all = Post("https://api.vk.com/method/groups.getMembers", "group_id=" + VkGid + "&count=1000&offset=" + offset + "&fields=last_seen&v=5.74&access_token=" + VkToken);
+                        Parse(all);
+                        Thread.Sleep(350);
+
+                    }
+                    if (Lst.Count > 0)
+                    {
+                        var msg = MessageBox.Show("Вы действительно хотите удалить " + Lst.Count + " собак из группы?", "Внимание!", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                        if (msg == DialogResult.OK)
+                        {
+                            progressBar1.Value = 0;
+                            progressBar1.Maximum = Lst.Count;
+                            foreach (var line in Lst)
+                            {
+                                var rem = Post("https://api.vk.com/method/groups.removeUser", "group_id=" + VkGid + "&user_id=" + line + "&v=5.74&access_token=" + VkToken);
+                                if (!RemParse(rem))
+                                {
+                                    MessageBox.Show("Не удалось удалить одного или нескольких пользователей!", "Ошибка!");
+                                    break;
+                                }
+                                if (progressBar1.Value < progressBar1.Maximum)
+                                {
+                                    progressBar1.Value++;
+                                }
+                                Thread.Sleep(350);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Собак в группе не обнаружено!", "Информация!");
+                    }
+                    button3.Enabled = true;
+
+
+                }).Start();
+            }
+            else
+            {
+                MessageBox.Show("Неверно заполнены поля ввода", "Ошибка");
+            }
+           
+        }
+        private void Parse(string all)
+        {
+            try
+            {
+                var obj = JObject.Parse(all);
+                var resp = obj["response"];
+                var cnt = resp["count"].Value<int>();
+                Usercount = cnt;
+                foreach(JToken line in resp["items"])
+                {
+                    var banned = line["deactivated"];
+                    if (banned != null && !string.IsNullOrWhiteSpace(banned.Value<string>()))
+                    {
+                        Lst.Add(line["id"].Value<string>());
+                    }
+                }
+                progressBar1.Maximum = (Usercount/1000+1);
+                if (progressBar1.Value < progressBar1.Maximum)
+                {
+                    progressBar1.Value++;
+                }
+            }
+            catch { Usercount = 0; }
+        }
+        private bool RemParse(string rem)
+        {
+            try
+            {
+                var obj = JObject.Parse(rem);
+                var resp = obj["response"];
+                if (resp.Value<string>() == "1")
+                {
+                    return true;
+                }
+            }
+            catch { }
+            return false;
+        }
+    }
+}
